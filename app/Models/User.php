bbd4939edfe689cb13466a0cedfb524489b58586
Ryan Hayden <pastorryanhayden@gmail.com>
2024-06-18 15:52:01 -0500

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
+
     use HasFactory, Notifiable;
 
     /**
@@ -44,4 +47,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
 }
